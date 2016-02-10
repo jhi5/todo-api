ed65100ecfe09a008c5944db1793e363922d5b43
J. Munford <jaemunford@gmail.com>
2016-02-10 17:04:26 -0500

    Basic API layer

--- server.js
+++ server.js
@@ -9,10 +9,37 @@ var todos = [{
 	id: 2
 	,description: "Buy food"
 	,completed: false
+},{
+	id: 3
+	,description: "Feed the cat"
+	,completed: true
 }];
 
 app.get("/", function(req, res){
 	res.send("Todo API Route");
+});
+
+app.get("/todos", function(req, res){
+	res.json(todos);
+});
+
+app.get("/todos/:id", function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo;
+
+	todos.forEach(function(todo){
+		if(todoId === todo.id){
+			matchedTodo = todo;
+		}
+	});
+	if(matchedTodo){
+		res.json(matchedTodo);
+	}else{
+		res.status(404).send();
+	}
+
+	res.send("Asking for todo with id of " + req.params.id);
+
 });
 
 app.listen(PORT, function(){
