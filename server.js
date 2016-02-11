ca2e43c83faa3488920664c1f84e0f2e380635c4
J. Munford <jaemunford@gmail.com>
2016-02-11 11:57:26 -0500

    Added DELETE

--- server.js
+++ server.js
@@ -43,6 +43,19 @@ app.post("/todos", function(req, res){
 	res.json(body);
 });
 
+app.delete("/todos/:id", function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	if(!matchedTodo){
+		res.status(400).json("Error: No todo with that id");
+		return res.end();
+	}else{
+		todos = _.without(todos, matchedTodo);	
+		res.json(matchedTodo);
+	}
+	
+});
+
 app.listen(PORT, function(){
 	console.log("Express is listening on port " + PORT + "!");
 });
